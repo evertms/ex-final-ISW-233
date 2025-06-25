1b3ed6df340e99e2664044aa5f1e86c92f98bd99
Evert Moreno Serrate <evert23122004@gmail.com>
2025-06-25 11:50:48 -0400

    Footer web component

---
+++ frontend/blocks/footer/footer.js
@@ -0,0 +1,27 @@
+const template = document.createElement("template");
+template.innerHTML = `
+<link rel="stylesheet" href="./blocks/footer/footer.css">
+    <div class="social-icons">
+        <a href="https://www.instagram.com/evertms_/" target="_blank">
+            <img src="./assets/pngs/ig.png" alt="Instagram" width="40">
+        </a>
+
+        <a href="https://www.facebook.com/evert.morenoserrate.5/" target="_blank"> 
+            <img src="./assets/pngs/fb.png" alt="Facebook" width="40">                    
+        </a>
+
+        <a href="https://x.com/tuperfil" target="_blank" >
+            <img src="./assets/pngs/x.png" alt="Twitter" width="40">
+        </a>
+        
+    </div>    
+`
+
+class Footer extends HTMLElement{
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" }).appendChild(template.content.cloneNode(true));
+    }
+}
+
+customElements.define('footer-element', Footer);
