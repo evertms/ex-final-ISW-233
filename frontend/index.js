c9bc049341fa7e72637e3f7f8d6dfcf0d2ccdf6e
Evert Moreno Serrate <evert23122004@gmail.com>
2025-06-25 11:49:13 -0400

    Index.js, index.html y styles.css

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,17 @@
-import MyHeader from "./blocks/header/myHeader.js";
+import './blocks/side-bar/side-bar.js';
+import './blocks/header-bar/header-bar.js';
+import './blocks/menu-nav/menu-nav.js';
+import './blocks/menu-page/menu-page.js';
+import './blocks/reservation/reservation.js';
+import './blocks/about-page/about.js';
+import './blocks/footer/footer.js';
+import './blocks/contact/contact.js';
+import './blocks/blog/blog.js';
+import './blocks/registration/registration.js';
+import './blocks/login/login.js';
+import './blocks/cart/cart.js';
+import './blocks/blog-single/blog-single.js';
+
+import router from './services/router.js';
+
+router.init();
